faf9f6ef48dd15bab03992b0fd8c960bc131ddee
netop://ウエハ <paul@webb.page>
2019-07-24 20:26:31 -0500

    Updated deps

--- extensions.js
+++ extensions.js
@@ -5,18 +5,19 @@
 //  E X P O R T S
 
 module.exports = exports = [
+  // #
   "3g2",
   "3gp",
-
+  // A
   "aaf",
   "asf",
   "avchd",
   "avi",
-
+  // D
   "drc",
-
+  // F
   "flv",
-
+  // M
   "m2v",
   "m4p",
   "m4v",
@@ -30,24 +31,24 @@ module.exports = exports = [
   "mpg",
   "mpv",
   "mxf",
-
+  // N
   "nsv",
-
+  // O
   "ogg",
   "ogv",
-
+  // Q
   "qt",
-
+  // R
   "rm",
   "rmvb",
   "roq",
-
+  // S
   "svi",
-
+  // V
   "vob",
-
+  // W
   "webm",
   "wmv",
-
+  // Y
   "yuv"
 ];
